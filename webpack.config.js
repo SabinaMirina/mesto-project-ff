cabb8338d88036b92d7d3941fa1fab0850f8c49d
Sabina Mirina <mirinasabina@mail.ru>
2024-06-27 14:58:37 +0200

    Save changes before switching branches

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,8 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
-    publicPath: "./",
+    publicPath: ".
+    /",
   },
   mode: "development",
   devServer: {
